0fde47bb0ae753bfd66d2f05083008673359eb1c
Taylor Seager <taylorseager718@gmail.com>
2024-08-26 21:39:32 -0500

    added dynamic files for product/order, added pages, updated with boilerplate code, routes working as expected

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
+  console.warn(user);
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -14,13 +15,14 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
+      <h1>Hello! </h1>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
       </Button>
     </div>
+
+  // TODO: Update to map through all products
   );
 }
 
